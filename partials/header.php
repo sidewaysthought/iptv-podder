c4e71e9f1bf93ee26393be25b52f57558362debc
Sideways Thought <92705102+sidewaysthought@users.noreply.github.com>
2025-05-17 17:30:57 -0500

    Move share button near load and use svg icon

--- partials/header.php
+++ partials/header.php
@@ -3,16 +3,6 @@
         <div class="font-semibold">View-IPTV.stream</div>
         <nav class="space-x-4 flex items-center">
             <a href="index.php" class="text-blue-600 hover:underline">Home</a>
-            <div class="relative">
-                <button id="shareBtn" aria-haspopup="true" aria-expanded="false" class="text-blue-600">
-                    <span aria-hidden="true">🔗</span>
-                    <span class="sr-only">Share</span>
-                </button>
-                <div id="shareMenu" class="absolute right-0 mt-2 w-48 bg-white border rounded shadow hidden">
-                    <button id="sharePlaylistBtn" class="block w-full text-left px-2 py-1 hover:bg-gray-100 disabled:opacity-50" disabled>Share playlist</button>
-                    <button id="shareVideoBtn" class="block w-full text-left px-2 py-1 hover:bg-gray-100 disabled:opacity-50" disabled>Share playlist &amp; video</button>
-                </div>
-            </div>
         </nav>
     </div>
 </header>
