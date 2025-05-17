d9611d4a7cc87bb6a54415424b4767dd523bb9ef
Sideways Thought <92705102+sidewaysthought@users.noreply.github.com>
2025-05-17 01:11:02 -0500

    Allow dynamic page titles via pageTitle variable

---
+++ partials/header.php
@@ -0,0 +1,8 @@
+<header class="bg-gray-200">
+    <div class="container mx-auto max-w-6xl flex justify-between items-center p-4">
+        <div class="font-semibold">View-IPTV.stream</div>
+        <nav class="space-x-4">
+            <a href="index.php" class="text-blue-600 hover:underline">Home</a>
+        </nav>
+    </div>
+</header>
